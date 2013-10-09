9cadae2dac83fe038ddb830b4461cc4e51ca58a0
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2013-10-09 12:37:58 -0300

    Remove 7 characters

--- mu.php
+++ mu.php
@@ -6,4 +6,4 @@
  * @author Stefano Azzolini <lastguest@gmail.com>
  */
 
-class µ{static function __callStatic($n,$a){static$r;return$n=='_'?@$r[getenv(REQUEST_METHOD)][getenv(REQUEST_URI)]():$r[$n][$a[0]]=$a[1];}}
+class µ{function __callStatic($n,$a){static$r;return$n=='_'?@$r[getenv(REQUEST_METHOD)][getenv(REQUEST_URI)]():$r[$n][$a[0]]=$a[1];}}
