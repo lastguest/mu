26bfb082ab7d3dcb5761ab46a5b513c1b82e316a
Stefano Azzolini <lastguest@gmail.com>
2013-10-10 10:41:49 +0200

    Changed from static module to function
    Applied `return` fix (@celtric)
    Changed from static module to function

--- mu.php
+++ mu.php
@@ -6,4 +6,4 @@
  * @author Stefano Azzolini <lastguest@gmail.com>
  */
 
-class µ{function __call($n,$a){static$r;return$n=='_'?@$r[getenv(REQUEST_METHOD).getenv(REQUEST_URI)]():$r[$n.$a[0]]=$a[1];}}
+function µ($a=0,$b=0){static$r;$a?$r[$a]=$b:@$r[getenv(REQUEST_METHOD).' '.getenv(REQUEST_URI)]();}
