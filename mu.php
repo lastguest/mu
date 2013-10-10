36f57129ef6e569a82c7c7fc9a59026d778f2f9e
Stefano Azzolini <lastguest@gmail.com>
2013-10-10 02:09:51 +0200

    Update mu.php

--- mu.php
+++ mu.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * µ PHP microframework
+ * µ PHP microframework - Hardcore Version
  *
  * @author Stefano Azzolini <lastguest@gmail.com>
  */
 
-class µ{static function __callStatic($n,$a){static$r;return$n=='_'?@$r[getenv(REQUEST_METHOD).getenv(REQUEST_URI)]():$r[$n.$a[0]]=$a[1];}}
+class µ{function __call($n,$a){static$r;return$n=='_'?@$r[getenv(REQUEST_METHOD).getenv(REQUEST_URI)]():$r[$n.$a[0]]=$a[1];}}
